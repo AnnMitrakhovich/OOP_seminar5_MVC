41e12d7355cc64bf9325a02453dc4d0396e08503
Ann <ann.mitrakhovich@gmail.com>
2023-11-27 10:46:42 +0300

    changed data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import Controler.Controler;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Controler controler = new Controler();
+        controler.createStudent("Петров", "Иван","Сергеевич","2010");
+        controler.createStudent("Сидоров", "Павел","Сергеевич","2011");
+        controler.getAllStudent();
+        controler.createStudent("Антонов", "Андрей","Васильевич","2010");
+        controler.createStudent("Федоров", "Иван","Петрович","2009");
+        controler.getAllStudent();
     }
 }
